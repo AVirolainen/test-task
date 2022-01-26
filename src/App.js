7b133e00728bc1db281ce83049c0d0d7e5e1d7db
Rudenko Stanislav <rudenkostas2@gmail.com>
2022-01-26 22:43:29 +0200

    init project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,106 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"
+import styles from './App.module.css';
+import { Observable, interval } from 'rxjs';
+import clock from "./img/clock.png"
 
 function App() {
+  const [time, setTime] = useState(0)
+  const [subscription, setSubscription] = useState()
+  const [state, setState] = useState("waiting")
+  const [doubleClick, setDoubleClick] = useState(0)
+  const [calculatedIntervals, setCalculatedIntervals] = useState([])
+
+  const observable = new Observable((observer) => {
+    interval(10)
+      .subscribe(val => {
+        observer.next(val)
+      })
+  })
+
+  const subscribe = () => {
+    return observable.subscribe({
+      next(x) {
+        setTime(x => x + 10)
+      }
+    })
+  }
+
+  const handleStart = () => {
+    setState("going")
+    setSubscription(subscribe())
+  }
+
+  const handleWait = () => {
+    let diff = new Date() - doubleClick
+    if (doubleClick === 0 || diff > 300) {
+      setDoubleClick(new Date())
+    } else {
+      subscription.unsubscribe()
+      setState("waiting")
+      setDoubleClick(0)
+    }
+  }
+
+  const handleStop = () => {
+    setState("waiting")
+    setCalculatedIntervals(calculatedIntervals.concat(time))
+    setTime(0)
+    subscription.unsubscribe()
+  }
+
+  const handleReset = () => {
+    setTime(0)
+    if (subscription.closed || subscription.isStopped) {
+      handleStart()
+    }
+  }
+
+  const calculateTime = (seconds) => {
+    return new Date(seconds).toISOString().slice(14, 22).replace(".", ":")
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.App}>
+      <div className={styles.container}>
+        <div className={styles.header}>
+          Timer
+        </div>
+        <div className={styles.timer}>
+          <img src={clock} alt={clock} className={styles.clockImage} />
+          {calculateTime(time)}
+
+        </div>
+        <div className={styles.buttonsBlock}>
+          {state === "waiting" ?
+            <button type="button" onClick={handleStart} className={styles.taskButton}>
+              Start
+            </button> :
+            <button type="button" onClick={handleStop} className={styles.taskButton}>
+              Stop
+            </button>
+          }
+          <button type="button" onClick={handleWait} className={styles.taskButton}>
+            Wait
+          </button>
+          <button type="button" onClick={handleReset} className={styles.taskButton}>
+            Reset
+          </button>
+        </div>
+      </div>
+      <div className={styles.timesContainer}>
+        <div className={styles.timesHeader}>
+          Calculated Times
+          <div className={styles.timesBlock}>
+            {
+              calculatedIntervals.map((item, index) => {
+                return (<div key={item}>
+                  {index + 1}: {calculateTime(item)}
+                </div>)
+              })
+            }
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
